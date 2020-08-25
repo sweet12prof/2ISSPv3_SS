a74c3041716318c57862bbf68d3a7bfa217f6141
unknown <chrisnut12@gmail.com>
2020-08-25 03:19:48 +0000

    Progress

--- SourceFiles/Scheduler_cpp/_as_Instr_/I_asInstr_1.cpp
+++ SourceFiles/Scheduler_cpp/_as_Instr_/I_asInstr_1.cpp
@@ -13,18 +13,24 @@
         case Instructions::InstrType::R_Type :
             R_Instruction * Instr2_derivedPtr = dynamic_cast<R_Instruction *>(Instrpair.second);
             if(Instr1_derivedPtr->getRt() == Instr2_derivedPtr->getRs() || Instr1_derivedPtr->getRt() == Instr2_derivedPtr->getRt())
-                result =  std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
-                            nopInstruction(Instructions::machineFormat::Decimal), Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                result =  std::vector<std::string>{
+                            Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                            nopInstruction(Instructions::machineFormat::Decimal), 
+                            Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                        };
             else 
-                result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
-                            Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                result = std::vector<std::string>{
+                            Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
+                            Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                        };
         break;
 
         case Instructions::InstrType::J_Type :
             J_Instructions * Instr2_derivedPtr = dynamic_cast<J_Instructions *>(Instrpair.second);
-                result = std::vector<std::string>{ Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                result = std::vector<std::string>{ 
+                            Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
                             Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)  
-                            };
+                        };
         break;
 
         case Instructions::InstrType::I_Type:
@@ -49,23 +55,123 @@
                 }
             }
         break;
+
+        
     }
-     
+     return result;
 }
  
 
  std::vector<std::string> Lw_as_I_type_Type(I_Instruction * i1, I_Instruction *i2){
      if(i2->getIType_Type() == I_Instruction::I_Type::Mem_LwType || i2->getIType_Type() == I_Instruction::I_Type::Mem_Sw_Type)
-        return  std::vector<std::string>{i1->MachineCodeString(Instructions::machineFormat::Decimal), 
-                            nopInstruction(Instructions::machineFormat::Decimal), i2->MachineCodeString(Instructions::machineFormat::Decimal)};
+        return  std::vector<std::string>{
+                            i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                            nopInstruction(Instructions::machineFormat::Decimal), 
+                            i2->MachineCodeString(Instructions::machineFormat::Decimal)
+                        };
     else if(i2->getIType_Type() == I_Instruction::I_Type::Branch_Type)
             {
                 if(i1->getRt() == i2->getRs() || i1->getRt() == i2->getRt())
-                        return  std::vector<std::string>{ i1->MachineCodeString(Instructions::machineFormat::Decimal), nopInstruction(Instructions::machineFormat::Decimal), 
-                                                    i2->MachineCodeString(Instructions::machineFormat::Decimal), 
-                                    };
-
-            }
+                        return  std::vector<std::string>{ 
+                                i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                nopInstruction(Instructions::machineFormat::Decimal), 
+                                i2->MachineCodeString(Instructions::machineFormat::Decimal) 
+                            };
+                else
+                         return std::vector<std::string>
+                         {
+                             i2->MachineCodeString(Instructions::machineFormat::Decimal), 
+                             i1->MachineCodeString(Instructions::machineFormat::Decimal)  
+                        };
         
 
+            }
+    else {
+            if(i1->getRt() == i2->getRs() || i1->getRt() == i2->getRt())
+                return 
+                    std::vector<std::string>
+                                    { 
+                                        i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                        nopInstruction(Instructions::machineFormat::Decimal), 
+                                        i2->MachineCodeString(Instructions::machineFormat::Decimal)
+                                    };
+        else 
+                 return std::vector<std::string>
+                         {
+                             i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                             i2->MachineCodeString(Instructions::machineFormat::Decimal)  
+                        };
+    }
  }
+
+
+std::vector<std::string> Beq_as_I_Type_Type(I_Instruction * i1, I_Instruction *i2){
+    return std::vector<std::string>{ 
+                                i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                nopInstruction(Instructions::machineFormat::Decimal), 
+                                i2->MachineCodeString(Instructions::machineFormat::Decimal) 
+                            };   
+}
+
+
+std::vector<std::string> sw_as_I_type_Type(I_Instruction *i1, I_Instruction *i2){
+     if(i2->getIType_Type() == I_Instruction::I_Type::Mem_LwType || i2->getIType_Type() == I_Instruction::I_Type::Mem_Sw_Type)
+        return  std::vector<std::string>{
+                            i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                            nopInstruction(Instructions::machineFormat::Decimal), 
+                            i2->MachineCodeString(Instructions::machineFormat::Decimal)
+                        };
+     
+     else if(i2->getIType_Type() == I_Instruction::I_Type::Branch_Type) 
+        return std::vector<std::string>
+                         {
+                             i2->MachineCodeString(Instructions::machineFormat::Decimal), 
+                             i1->MachineCodeString(Instructions::machineFormat::Decimal)  
+                        };
+    
+     else 
+         return std::vector<std::string>
+                         {
+                             i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                             i2->MachineCodeString(Instructions::machineFormat::Decimal)  
+                        };
+}
+
+
+std::vector<std::string> Imm_as_I_Type_Type(I_Instruction *i1, I_Instruction *i2){
+    std::vector<std::string> result; 
+    switch(i2->getIType_Type()){
+        case I_Instruction::I_Type::Mem_LwType:
+        case I_Instruction::I_Type::Mem_Sw_Type:
+        case I_Instruction::I_Type::Imm_Type :
+            if(i1->getRt() == i2->getRs() || i1->getRt() == i2->getRt())
+                result = std::vector<std::string>{
+                            i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                            nopInstruction(Instructions::machineFormat::Decimal), 
+                            i2->MachineCodeString(Instructions::machineFormat::Decimal)
+                        };
+            else 
+                result = std::vector<std::string>{
+                            i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                            i2->MachineCodeString(Instructions::machineFormat::Decimal)
+                        };
+
+        case I_Instruction::I_Type::Branch_Type :
+                 if(i1->getRt() == i2->getRs() || i1->getRt() == i2->getRt())
+                        return  std::vector<std::string>{ 
+                                i1->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                nopInstruction(Instructions::machineFormat::Decimal), 
+                                i2->MachineCodeString(Instructions::machineFormat::Decimal) 
+                            };
+                else
+                         return std::vector<std::string>
+                         {
+                             i2->MachineCodeString(Instructions::machineFormat::Decimal), 
+                             i1->MachineCodeString(Instructions::machineFormat::Decimal)  
+                        };
+        break;
+                
+    }
+
+    return result;
+}
--- SourceFiles/Scheduler_cpp/_as_Instr_/R_as_Instr1.cpp
+++ SourceFiles/Scheduler_cpp/_as_Instr_/R_as_Instr1.cpp
@@ -11,11 +11,16 @@ std::vector<std::string> Scheduler::R_asInstr_1(){
                 R_Instruction * Instr2_derivedPtr = dynamic_cast<R_Instruction *>(Instrpair.second);
                 if( (Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRs()) && (Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRt()) && 
                         (Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRd()) )
-                    result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
-                            nopInstruction(Instructions::machineFormat::Decimal), Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                    result = std::vector<std::string>{
+                                Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                nopInstruction(Instructions::machineFormat::Decimal), 
+                                Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                            };
                 else 
-                     result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
-                            Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                     result = std::vector<std::string>{
+                                Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
+                                Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                            };
            }
         break;
 
@@ -29,42 +34,58 @@ std::vector<std::string> Scheduler::R_asInstr_1(){
                     case I_Instruction::I_Type::Imm_Type : 
                         if(Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRs() || Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRt())
                             
-                           result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
-                            nopInstruction(Instructions::machineFormat::Decimal), Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                           result = std::vector<std::string>{
+                                        Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                        nopInstruction(Instructions::machineFormat::Decimal), 
+                                        Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                                    };
                         else 
-                            result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
-                            Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                            result = std::vector<std::string>{
+                                        Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
+                                        Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                                    };
                     break;
 
 
                     case I_Instruction::I_Type::Branch_Type :
                         if(Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRs() || Instr1_derivedPtr->getRd() == Instr2_derivedPtr->getRt())
-                            result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
-                                nopInstruction(Instructions::machineFormat::Decimal), Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};          
+                            result = std::vector<std::string>{
+                                    Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                    nopInstruction(Instructions::machineFormat::Decimal), 
+                                    Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                                };          
                         else 
-                            result = std::vector<std::string>{Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),
-                                Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
+                            result = std::vector<std::string>{
+                                    Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),
+                                    Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
                                 };
                     break;
 
                     default: 
-                           result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
-                            nopInstruction(Instructions::machineFormat::Decimal), Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)};
+                           result = std::vector<std::string>{
+                                        Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                        nopInstruction(Instructions::machineFormat::Decimal), 
+                                        Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal)
+                                    };
                 }
             }
         break;
 
         case Instructions::InstrType::J_Type : {
                 J_Instructions * Instr2_derivedPtr = dynamic_cast<J_Instructions *>(Instrpair.second);
-                result = std::vector<std::string>{Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),
-                        Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
+                    result = std::vector<std::string>{
+                                Instr2_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),
+                                Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal),  
                             };
         }
         break;
 
          default: 
-                result = std::vector<std::string>{Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
-                nopInstruction(Instructions::machineFormat::Decimal),  nopInstruction(Instructions::machineFormat::Decimal)};
+                result = std::vector<std::string>{
+                                Instr1_derivedPtr->MachineCodeString(Instructions::machineFormat::Decimal), 
+                                nopInstruction(Instructions::machineFormat::Decimal),  
+                                nopInstruction(Instructions::machineFormat::Decimal)
+                            };
             break;
     }
 
